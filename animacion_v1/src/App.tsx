909c0a3c5e88efc934782f6006ae5eb3590ebe2b
rpcieszaidin <rauljimenez@ieszaidinvergeles.org>
2025-01-13 22:59:46 +0100

    a

---
+++ animacion_v1/src/App.tsx
@@ -0,0 +1,50 @@
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import Tamagotchi, { TamagotchiProps } from './components/Tamagotchi';
+import { GameStore } from './screens/store/store';
+import { Directions } from './entities/Player';
+
+const App = () => {
+    const initStore = GameStore((state) => state.init);
+    const players = GameStore((state) => state.tamagotchis);
+    const move = GameStore((state) => state.move);
+    
+    const movePlayer = (name: String , direction: Directions) => { 
+        move(name,direction);
+    } 
+
+    useEffect(() => {
+        initStore();
+    }, [])
+    
+
+    return (
+        <SafeAreaView>
+            <View style={styles.overlay}>
+                <Button title='Derecha' onPress={ ()=> { movePlayer("player1", Directions.Right ) }}/>
+                <Button title='Izquierda' onPress={ ()=> { movePlayer("player1", Directions.Left ) }}/>
+                <Button title='Arriba' onPress={ ()=> { movePlayer("player1", Directions.Up ) }}/>
+                <Button title='Abajo' onPress={ ()=> { movePlayer("player1", Directions.Down ) }}/>
+            </View>
+            {
+                players.map( (item)=> (
+                    <Tamagotchi key={item.name.toString()} active={item.active} name={item.name} link={null}/>
+                ))
+            }
+        </SafeAreaView>
+    )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+    overlay: {
+        position: "absolute",
+        top: 500,
+        right: 0,
+        backgroundColor: "rgba(0, 0, 0, 0.9)",
+        alignItems: "center",
+        zIndex: 10
+      }
+})
+
